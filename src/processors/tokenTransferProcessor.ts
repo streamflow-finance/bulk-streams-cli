61306afc0dd27aa5a004dc998360fddfe3b5c560
Yolley <Yolley@users.noreply.github.com>
2024-04-03 13:27:12 +0900

    reuse confirmation strategy for token transfers from vesting

--- src/processors/tokenTransferProcessor.ts
+++ src/processors/tokenTransferProcessor.ts
@@ -1,9 +1,11 @@
+import bs58 from "bs58";
 import {
   createTransferCheckedInstruction,
   getAssociatedTokenAddress,
 } from "@solana/spl-token";
-import { ComputeBudgetProgram, Connection, Keypair, PublicKey, Transaction } from "@solana/web3.js";
+import { ComputeBudgetProgram, Connection, Keypair, PublicKey, Transaction, SendTransactionError, TransactionExpiredBlockheightExceededError } from "@solana/web3.js";
 import { getBN } from "@streamflow/stream";
+import { confirmAndEnsureTransaction } from "@streamflow/common/solana";
 
 import { IRecipientInfo } from "../utils/recipientStream";
 import { getOrCreateAssociatedTokenAccount } from "../utils/spl";
@@ -38,14 +40,37 @@ export const processTokenTransfer = async (
   }
   tx.add(ix);
   tx.partialSign(sender);
-  const signature = await connection.sendRawTransaction(tx.serialize(), { maxRetries: 3 });
-  const res = await connection.confirmTransaction({
-    blockhash: recentBlockInfo.blockhash,
-    lastValidBlockHeight: recentBlockInfo.lastValidBlockHeight + 50,
-    signature
-  }, "confirmed");
-  if (res.value.err) {
-    throw new Error(res.value.err.toString());
+  let signature = bs58.encode(tx.signature!);
+  try {
+    signature = await connection.sendRawTransaction(tx.serialize(), { maxRetries: 3 });
+  } catch (err) {
+    if (err instanceof SendTransactionError && err.message.includes("Blockhash not found")) {
+      console.log(`\n${recipientInfo.address}: Got 'Blockhash not found', will validate the transaction landing in 3 seconds...`)
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    } else {
+      throw err;
+    }
+  }
+  try {
+    const res = await connection.confirmTransaction({
+      blockhash: recentBlockInfo.blockhash,
+      lastValidBlockHeight: recentBlockInfo.lastValidBlockHeight + 50,
+      signature
+    }, "confirmed");
+    if (res.value.err) {
+      throw new Error(res.value.err.toString());
+    }
+  } catch (e) {
+    // If BlockHeight expired, we will check tx status one last time to make sure
+    if (e instanceof TransactionExpiredBlockheightExceededError) {
+      console.log(`\n${recipientInfo.address}: Got 'BlockHeight expired', will try to confirm anyway in 3 seconds...`)
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      const value = await confirmAndEnsureTransaction(connection, signature);
+      if (!value) {
+        throw e;
+      }
+    }
+    throw e;
   }
   return { txId: signature };
 };
